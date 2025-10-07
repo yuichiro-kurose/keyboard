5bb96973b81146fabe0ef8b69f7e7c534457acbd
Yuichiro Kurose <mathmouse1111@gmail.com>
2025-10-07 15:47:08 +0900

    20251007

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <algorithm>
 #include <climits>
 #include <iostream>
+#include <queue>
 #include <string>
+#include <utility>
 #include <vector>
 
 // ドキュメントを読み込む関数
@@ -22,6 +24,24 @@ std::string LoadDoc() {
     }
   }
   return doc;
+}
+
+// 出現頻度の順を表す配列を構築する関数
+std::vector<int> BuildRank(std::string doc) {
+  std::vector<std::pair<int, int>> count(26);
+  for (int i = 0; i < 26; i++) {
+    count[i] = std::make_pair(0, i);
+  }
+  for (char c : doc) {
+    count[c - '0'].first++;
+  }
+  std::sort(count.begin(), count.end());
+  std::reverse(count.begin(), count.end());
+  std::vector<int> rank(26);
+  for (int i = 0; i < 26; i++) {
+    rank[i] = count[i].second;
+  }
+  return rank;
 }
 
 // 頂点集合を{a,b,...,z}とする重み付き完全グラフを構築する関数
@@ -65,15 +85,16 @@ std::vector<std::vector<int>> SplitKey(std::vector<std::vector<int>> weight) {
   }
   std::vector<std::vector<int>> keyboard(2);
   for (int i = 0; i < 26; i++) {
-    keyboard[(best_combination >> i) & 1].push_back(i);
+    int hand = (best_combination >> 1) & 1;
+    keyboard[hand].push_back(i);
   }
   return keyboard;
 }
 
 // キーの配置を決定する関数
-void Place(std::vector<std::vector<int>>& keyboard, std::string doc) {
+void Place(std::vector<std::vector<int>>& keyboard, std::vector<int> rank) {
   // finger[i] := indexがiのキーを担当する指の番号
-  std::vector<int> finger = {4, 3, 2, 1, 1, 4, 3, 2, 1, 1, 4, 3, 2, 1, 1};
+  std::vector<int> finger = {3, 2, 1, 0, 0, 3, 2, 1, 0, 0, 3, 2, 1, 0, 0};
   // cost[i] := コストがiであるキーのindex
   std::vector<std::vector<int>> cost(5);
   cost[0] = {7, 8};
@@ -88,7 +109,30 @@ void Place(std::vector<std::vector<int>>& keyboard, std::string doc) {
         do {
           do {
             do {
-              // ここに処理を書く
+              // queue内で最も優先度が高いkeyが常に先頭
+              std::queue<int> priority;
+              for (int i = 0; i < 4; i++) {
+                for (int idx : cost[i]) {
+                  priority.push(idx);
+                }
+              }
+              // 今注目ている手のアルファベットのbit集合
+              int key = 0;
+              for (int idx : keyboard[hand]) {
+                key |= (1 << idx);
+              }
+              // combination[i] := 指iが担当するkey
+              std::vector<std::vector<int>> combination(5);
+              for (int i = 0; i < 26; i++) {
+                if ((key >> rank[i]) & 1) {
+                  int idx = priority.front();
+                  priority.pop();
+                  combination[finger[idx]].push_back(rank[i]);
+                }
+              }
+              // 2025年10月07日
+              // このループ内における配置での、指ごとの担当アルファベットを表す配列が完成
+              // あとは指の連続使用回数を数え、最小であったものを採用するプログラムを作成する
             } while (std::next_permutation(cost[4].begin(), cost[4].end()));
           } while (std::next_permutation(cost[3].begin(), cost[3].end()));
         } while (std::next_permutation(cost[2].begin(), cost[2].end()));
@@ -99,8 +143,9 @@ void Place(std::vector<std::vector<int>>& keyboard, std::string doc) {
 
 int main() {
   std::string doc = LoadDoc();
+  std::vector<int> rank = BuildRank(doc);
   std::vector<std::vector<int>> weight = BuildWeight(doc);
   std::vector<std::vector<int>> keyboard = SplitKey(weight);
-  Place(keyboard, doc);
+  Place(keyboard, rank);
   return 0;
 }
